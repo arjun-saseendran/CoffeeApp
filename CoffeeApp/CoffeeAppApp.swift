1933193392ca9844b2db334faaebefdc1db3ff6d
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-18 07:20:10 +0530

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Arjun Saseendran on 18/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
